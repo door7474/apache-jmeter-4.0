11bee696406772b5568580f9cfb514fb306d7a87
door7474 <34654119+door7474@users.noreply.github.com>
2018-04-22 14:51:12 +0800

    同步测试

--- src/core/org/apache/jmeter/my/myUtils.java
+++ src/core/org/apache/jmeter/my/myUtils.java
@@ -8,18 +8,15 @@ public class myUtils {
     //查询时间粒度工具方法
     public static long getGranularity(long granularity) {
         String my_report_granularity = JMeterUtils.getProperty("my_report_granularity");
-        System.out.println("hello");
+
         //为空返回默认时间粒度
         if (my_report_granularity == null) {
-            System.out.println("hello1");
             return granularity;
         }
         //非正整数回默认时间粒度
         if (! isNumeric(my_report_granularity)){
-            System.out.println("hello2");
             return granularity;
         }
-        System.out.println("hello3");
         //返回设置的时间粒度
         return Long.valueOf(my_report_granularity);
     }
